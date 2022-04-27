2a0e6bc7263798f5c3955dbad11822871006f98e
PatriciaPala <101339915+PatriciaPala@users.noreply.github.com>
2022-04-27 08:58:08 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    _function("abc")
+})
+function _function (text: string) {
+    basic.showString("badest")
+    basic.showIcon(IconNames.Heart)
+}
+basic.forever(function () {
+	
+})
